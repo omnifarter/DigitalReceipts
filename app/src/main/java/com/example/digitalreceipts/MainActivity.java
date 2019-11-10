77a5d669be5a713436e2f61398e5a3c65d5f7bee
Gabriel <omnifarter@gmai;.com>
2019-11-11 00:02:52 +0800

    commit from desktop test 1

--- app/src/main/java/com/example/digitalreceipts/MainActivity.java
+++ app/src/main/java/com/example/digitalreceipts/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.digitalreceipts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+//test push from desktop
 public class MainActivity extends AppCompatActivity {
 
     @Override

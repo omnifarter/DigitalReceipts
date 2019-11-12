d83211379320aa65c2ba1da7b79562bbe8f8ba2d
Gabriel <omnifarter@gmail.com>
2019-11-12 21:22:23 +0800

    1st commit with nav_bar implemented

--- app/src/main/java/com/example/digitalreceipts/MainActivity.java
+++ app/src/main/java/com/example/digitalreceipts/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.digitalreceipts;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 //test push from desktop
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
     }
 }
+

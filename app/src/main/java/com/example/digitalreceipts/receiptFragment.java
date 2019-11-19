792867c84453af8f2d542b50f5a6be082e62abb1
Crystal Yeoh <crystalyeohje@berkeley.edu>
2019-11-19 13:43:25 +0800

    Contacts List Appears

--- app/src/main/java/com/example/digitalreceipts/receiptFragment.java
+++ app/src/main/java/com/example/digitalreceipts/receiptFragment.java
@@ -9,6 +9,7 @@ import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
@@ -115,15 +116,17 @@ public class receiptFragment extends Fragment {
                 split_bill.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.READ_CONTACTS) == PackageManager.PERMISSION_GRANTED) {
-                            Log.i("receipt fragment", "bill is splitted");
+                        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+                            Log.i("receipt fragment", "bill is splitTed");
                             Toast.makeText(getContext(), "bill spitting", Toast.LENGTH_LONG).show();
                             Intent intent = new Intent(getActivity(), ContactsActivity.class);
                             intent.putExtra(BILL_KEY, receipts);
                             startActivity(intent);
-                        }else{
+                        } else {
                             requestStoragePermission();
                         }
+
+
                     }
                 });
                 int width = (int) (rootView.getMeasuredWidth() * 0.8);
@@ -164,8 +167,10 @@ public class receiptFragment extends Fragment {
 
     }
 
+
     private void requestStoragePermission() {
         if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), Manifest.permission.READ_CONTACTS)) {
+            Toast.makeText(getActivity(), "Permission box coming out", Toast.LENGTH_SHORT).show();
             new AlertDialog.Builder(getActivity())
                     .setTitle("Permission needed")
                     .setMessage("I need permission")
@@ -174,26 +179,31 @@ public class receiptFragment extends Fragment {
                         public void onClick(DialogInterface dialog, int which) {
                             ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.READ_CONTACTS}, STORAGE_PERMISSION_CODE);
                         }
-                    }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    dialog.dismiss();
-                }
-            })
+                    })
+                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    })
                     .create().show();
         } else {
+            Log.i("permissions", "asking for permission");
             ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.READ_CONTACTS}, STORAGE_PERMISSION_CODE);
         }
+
     }
+
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-            if(requestCode==STORAGE_PERMISSION_CODE){
-                if(grantResults.length>0&&grantResults[0]==PackageManager.PERMISSION_GRANTED){
-                    Toast.makeText(getActivity(),"Permission GRANTED",Toast.LENGTH_SHORT).show();
-                }else{
-                    Toast.makeText(getActivity(),"Permission DENIED",Toast.LENGTH_SHORT).show();
-                }
+        if (requestCode == STORAGE_PERMISSION_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(getActivity(), "Permission GRANTED", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getActivity(), "Permission DENIED", Toast.LENGTH_SHORT).show();
             }
+        }
     }
 }
+

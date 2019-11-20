ee7fd98dfc95fb5c2f2f5d323720164cd3c352c2
dangn511 <41414735+dangn511@users.noreply.github.com>
2019-11-21 01:22:33 +0800

    reupload with camera

--- app/src/main/java/com/google/sample/cloudvision/MainActivity.java
+++ app/src/main/java/com/google/sample/cloudvision/MainActivity.java
@@ -62,9 +62,6 @@ import java.io.FileReader;
 import java.util.Iterator;
 import java.util.Map;
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.*;
 
 
 public class MainActivity extends AppCompatActivity {
